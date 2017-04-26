b7039c7d8237bc7afb10410be8f8c049d3f1d105
David C <dchun@utexas.edu>
2017-04-26 14:32:43 -0500

    first push

---
+++ src/assignment7/ChatRoom_Observer.java
@@ -0,0 +1,19 @@
+package assignment7;
+
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.Observable;
+import java.util.Observer;
+
+public class ChatRoom_Observer extends PrintWriter implements Observer {
+	public ChatRoom_Observer(OutputStream out) {
+		super(out);
+	}
+	
+	@Override
+	public void update(Observable o, Object arg) {
+		this.println(arg); //writer.println(arg);
+		this.flush(); //writer.flush();
+	}
+
+}
